295d0c39464580b83c1d65630825bf53e826f2e8
devszark <devszark@gmail.com>
2022-10-19 13:03:05 +0200

    First commit

---
+++ src/main/java/com/example/kasa1/Kasa1Application.java
@@ -0,0 +1,13 @@
+package com.example.kasa1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kasa1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kasa1Application.class, args);
+	}
+
+}
